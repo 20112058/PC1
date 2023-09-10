d42a6bc4bb3094e5186cacc093df121a13aa36f5
Daniel <143299321+20112058@users.noreply.github.com>
2023-09-09 20:48:04 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Obtén el botón por su ID
+const botonRedireccionar1 = document.getElementById("botonSC");
+const botonRedireccionar2 = document.getElementById("botonAL");
+const botonRedireccionar3 = document.getElementById("botonU");
+const botonRedireccionar4 = document.getElementById("botonSBA");
+
+// Agrega un evento de click al botón
+botonRedireccionar1.addEventListener("click", function () {
+  // Redirecciona a la página web deseada
+  window.location.href = "https://es.wikipedia.org/wiki/Club_Sporting_Cristal"; // Reemplaza con la URL de la página a la que deseas redireccionar
+});
+
+botonRedireccionar2.addEventListener("click", function () {
+  // Redirecciona a la página web deseada
+  window.location.href = "https://es.wikipedia.org/wiki/Club_Alianza_Lima"; // Reemplaza con la URL de la página a la que deseas redireccionar
+});
+botonRedireccionar3.addEventListener("click", function () {
+  // Redirecciona a la página web deseada
+  window.location.href =
+    "https://es.wikipedia.org/wiki/Club_Universitario_de_Deportes"; // Reemplaza con la URL de la página a la que deseas redireccionar
+});
+botonRedireccionar4.addEventListener("click", function () {
+  // Redirecciona a la página web deseada
+  window.location.href = "https://es.wikipedia.org/wiki/Sport_Boys_Association"; // Reemplaza con la URL de la página a la que deseas redireccionar
+});
